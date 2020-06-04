06bc5e0459cecd1b0ccb60ec372b6ed77e5b653c
Michael McEniry <mcenirm@mceniry.net>
2020-06-04 21:45:42 +0000

    Fix example

--- example.cpp
+++ example.cpp
@@ -1,18 +1,37 @@
+#include <algorithm>
+#include <complex>
+#include <iostream>
+
 #include <fftw3.h>
 
-const int N = 256;
 
 int main(int ac, char** av)
 {
-    fftw_complex *in, *out;
-    fftw_plan p;
+    const int N = 256;
 
-    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    std::complex<double>* in  = new std::complex<double>[N];
+    for (int w = N / 4, i = 0, f = 1; i < N; i += w, f *= -1)
+    {
+        std::fill(in + i, in + i + w, std::complex<double>(1.0 * f, 0.0));
+    }
 
-    fftw_execute(p); /* repeat as needed */
+    std::complex<double>* out = new std::complex<double>[N];
 
+    fftw_plan p = fftw_plan_dft_1d(
+        N,
+        reinterpret_cast<fftw_complex*>(in),
+        reinterpret_cast<fftw_complex*>(out),
+        FFTW_FORWARD,
+        FFTW_ESTIMATE
+    );
+    fftw_execute(p);
     fftw_destroy_plan(p);
-    fftw_free(in); fftw_free(out);
+
+    delete [] in;
+
+    for (int i = 0; i < N; i++)
+    {
+        std::cout << out[i] << std::endl;
+    }
+    delete [] out;
 }
